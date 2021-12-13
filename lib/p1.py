de4b23be8fb3855874d0f92714acf43cd5fbd866
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2021-12-13 23:08:52 +0700

    add p1 solution

--- lib/p1.py
+++ lib/p1.py
@@ -1,3 +1,4 @@
 def find_split_80(integer_list):
     integer_list.sort()
+    # It should be: return integer_list[16]
     return integer_list[17]
--- test/p1-test.py
+++ test/p1-test.py
@@ -3,18 +3,16 @@ import unittest
 
 from lib import p1
 
-
-
 class Test_FindSplit80(unittest.TestCase):
     def test_1(self):
         list = range(0, 20)
+        print "\n[test_1] list: ", list
+        print "The first 80% are: ", range(0, 16)
+        print "The result should be: ", 16
+        print "The function return: ", p1.find_split_80(list)
         test = p1.find_split_80(list)
-        expected_result = 17
+        expected_result = 16
         self.assertEqual(test, expected_result)
 
-    def test_2(self):
-        
-
 if __name__ == '__main__':
-
     unittest.main(verbosity=2)
